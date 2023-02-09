8a8035692a908fe3a96ebe097b449204c4ebc386
carson_moon <carsonmoonatUCF@gmail.com>
2023-02-09 18:25:21 -0500

    Working enemy

--- Nova Drift Remix/Assets/Scripts/Projectiles/Movement_EnemyProjectile.cs
+++ Nova Drift Remix/Assets/Scripts/Projectiles/Movement_EnemyProjectile.cs
@@ -10,12 +10,14 @@ public class Movement_EnemyProjectile : MonoBehaviour
     private Rigidbody2D rb = null;
 
     // Projectile Target
-    private Transform target = null;
+    public Vector3 shotTarget = Vector3.zero;
 
     // Projectile Movement
     [Header("Movement")]
     public float startSpeed = 0.0f;
     public float travelDuration = 0.0f;
+    public float rotationSpeed = 0.0f;
+    private Vector3 smoothVelocity = Vector3.zero;
     
 
     private void Awake() {
@@ -28,32 +30,31 @@ public class Movement_EnemyProjectile : MonoBehaviour
 
     private void Update() {
         transform.position += transform.up * startSpeed * Time.deltaTime;
+        startSpeed += Time.deltaTime;
+    }
+
+    public void GetShotTarget(Vector3 target){
+        shotTarget = target;
     }
 
     IEnumerator ProjectileMovement(){
-        yield return new WaitForSeconds(1f);
-
-        
-
-
-
-
-
-        /*/
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1f);    
 
         float travelTime = 0;
-        Quaternion startValue = transform.rotation;
-        Quaternion endValue = Quaternion.Euler(0, 0, transform.rotation.z + 180);
 
         while(travelTime < travelDuration){
-            transform.rotation = Quaternion.Lerp(startValue, endValue, travelTime / travelDuration);
+
+            Vector3 direction = shotTarget - transform.position;
+
+            float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.z, targetAngle, ref smoothVelocity.y, rotationSpeed);
+
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
+            
             travelTime += Time.deltaTime;
-            startSpeed += Time.deltaTime * 2;
+            //startSpeed += Time.deltaTime;
             yield return null;
         }
-
-        transform.rotation = endValue;
-        /*/
     }
 }
