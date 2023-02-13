02b228e27ba73ab34331ffd2e8916e7bdd1f315c
carson_moon <carsonmoonatUCF@gmail.com>
2023-02-12 20:52:42 -0500

    Bug Fixes
    -player ui is working

--- Nova Drift Remix/Assets/Scripts/Game Managers/PlayerCheck.cs
+++ Nova Drift Remix/Assets/Scripts/Game Managers/PlayerCheck.cs
@@ -10,5 +10,11 @@ public class PlayerCheck : MonoBehaviour
         if(player){
             Destroy(player);
         }
+
+        GameObject playerUI = FindObjectOfType<HealthBars>().gameObject;
+
+        if(playerUI){
+            Destroy(playerUI);
+        }
     }
 }
