cef5af9f221e1c65d7effd36f4efa1453659b3f1
James Soth <97543733+Blusions@users.noreply.github.com>
2023-02-12 19:01:42 -0500

    Scene Fade In and Out

--- Nova Drift Remix/Assets/Scripts/Menu/LoadLevel2.cs
+++ Nova Drift Remix/Assets/Scripts/Menu/LoadLevel2.cs
@@ -5,19 +5,49 @@ using UnityEngine.SceneManagement;
 
 public class LoadLevel2 : MonoBehaviour
 {
+    public Animator animator;
+    
+    private int levelToLoad;
+    
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
     public void LoadLevel()
     {
         SceneManager.LoadScene(4);
+        FadeToLevel(4);
     }
 
     public void LoadTitleScreen()
     {
         SceneManager.LoadScene(0);
+        FadeToLevel(0);
     }
 
     public void LoadLevel1()
     {
         SceneManager.LoadScene(1);
+        FadeToLevel(1);
     }
+
+        public void LoadCoderLevel()
+    {
+        SceneManager.LoadScene(5);
+        FadeToLevel(5);
+    }
+
+        public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 
 }
